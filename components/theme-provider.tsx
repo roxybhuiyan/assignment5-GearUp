311940d8cc45e3ec1d187c10e17fb178bbdc95ae
Roxy <roxy@Roxys-MacBook-Pro.local>
2026-07-31 02:10:43 +0600

    theme change

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -3,6 +3,7 @@
 
 
 // ThemeProvider.tsx
+// This component is a wrapper around the NextThemesProvider from the next-themes package. It allows you to provide theme-related context to your application, enabling features like dark mode and theme switching.
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import type { ComponentProps } from "react";
 
