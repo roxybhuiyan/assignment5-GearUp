4757148d26e29f89c4de38f9649282a420dd1301
Roxy <roxy@Roxys-MacBook-Pro.local>
2026-08-01 22:23:50 +0600

    fixed some bugs

--- app/gear/[id]/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { DetailSkeleton } from "@/components/common/skeletons";
-
-export default function Loading() {
-  return (
-    <div className="mx-auto max-w-6xl px-4 py-10">
-      <DetailSkeleton />
-    </div>
-  );
-}
